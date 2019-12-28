beccda1b8cec8f7e3580aaa12ae01a36c713f60e
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2019-12-28 16:52:38 +0200

    authentication and participant listing

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,9 +20,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // no real security at the moment
+
+        http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
+        
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/h2-console/*").permitAll()
+                .anyRequest().authenticated();
+         http.formLogin()
+                .permitAll();
     }
 
     @Autowired
